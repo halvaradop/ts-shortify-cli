dd350f0f5f8db92fe5167a744eb21df74284fea4
Hernan Alvarado <105169289+halvaradop@users.noreply.github.com>
2024-06-13 23:06:56 -0500

    chore: Setup CLI configuration with options and update package.json (#1)
    Sets up the configuratin of the CLI with options for the future functionality. Some of the changes include:
    
    - Added options to CLI for future features
    - Updated package.json with dependencies and scripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
 #! /usr/bin/env node
+
+import "dotenv/config"
+import { Command } from "commander"
+
+
+
+/**
+ * Declare and initialize the program
+ */
+const program = new Command()
+
+
+program
+    .name("shortify url")
+    .description("Shorten your favorite URL using the CLI.")
+    .version("0.0.1")
+
+
+
+/**
+ * Configuration of CLI options and arguments
+ */
+program
+    .argument("<source-url>", "URL to shorten")
+    .option("-s, --stats", "Show the statistics for a URL")
+    .option("-u, --update", "Update the current link")
+    .option("-d, --delete", "Delete the current link")
+    .option("-dm, --domain <domain>", "Insert your custom domain")
+    .option("-v, --views", "Show the views by a short link")
+    .option("-l, --long", "Show the long version of a link");
+
+
+/**
+ * Parse the command line arguments
+ */
+program.parseAsync(process.argv)
